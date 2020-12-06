4ac16eea177027a1808620629791d58ec920ed25
wu383933588lyy <wu383933588lyy@163.com>
2020-12-06 10:52:05 +0800

    项目初始化

---
+++ 2020/src/main/java/com/cherry/december/LargestNumber.java
@@ -0,0 +1,11 @@
+package com.cherry.december;
+
+/**
+ * @ClassName LargestNumber
+ * @Author WuRui
+ * @Date 2020/12/6 10:50
+ * @Version 1.0
+ * @Description //TODO 16.07.最大数值
+ **/
+public class LargestNumber {
+}
